a3daae132762d06acd185ff47167a24fd64bd64c
Maria Muhammad Hussain <wma9969@outlook.com>
2021-05-28 18:56:50 +0500

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,8 +11,50 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text("Maria Muhammad Hussain"),
+        appBar: AppBar(
+          title: Center(
+            child: Text('Web & Mobile App Development'),
+          ),
+        ), //AppBar
+        body: Container(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    )), //Container
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'User Name'),
+                  ),
+                ), //Container
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ), //Container
+                SizedBox(height: 30),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: Text('Login'),
+                      onPressed: () {},
+                    )), //Container
+              ],
+            )), //Container
       ),
-    );
+    ); //MaterialApp
   }
 }
